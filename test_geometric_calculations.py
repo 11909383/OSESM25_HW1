f8b3f063df70966728e05e10468be0b9151f3d73
11909383 <e11909383@student.tuwien.ac.at>
2025-03-24 00:30:02 +0100

    create test file

---
+++ test_geometric_calculations.py
@@ -0,0 +1,20 @@
+from utils import triangular_surface_area
+from utils import circular_surface_area
+from utils import cylinder_volume
+
+def test_triangular_surface_area():
+    assert triangular_surface_area(1, 2) == 1
+    assert triangular_surface_area(12, 20) == 120
+    assert triangular_surface_area(4, 10) == 20
+
+
+def test_circular_surface_area():
+    assert circular_surface_area(4) == 50.265472
+    assert circular_surface_area(13) == 530.929048
+    assert circular_surface_area(99) == 30790.743192
+
+
+def test_cylinder_volume():
+    assert cylinder_volume(2, 5) == 62.83184
+    assert cylinder_volume(3, 8) == 226.194624
+    assert cylinder_volume(9, 7) == 1781.282664
